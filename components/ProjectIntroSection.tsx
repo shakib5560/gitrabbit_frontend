c1c931ac3f5e37e98ba340f2bd7cfdc9d30b9555
Sheikh Shamiul Shakib <dev.shakib24@gmail.com>
2026-08-04 12:27:07 +0600

    chore: perfect landing page 3.0

--- components/ProjectIntroSection.tsx
+++ components/ProjectIntroSection.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import { Play } from "lucide-react";
 
-const containerVariants = {
+const containerVariants: Variants = {
   hidden: { opacity: 0 },
   show: {
     opacity: 1,
@@ -11,7 +11,7 @@ const containerVariants = {
   },
 };
 
-const itemVariants = {
+const itemVariants: Variants = {
   hidden: { opacity: 0, y: 30 },
   show: { opacity: 1, y: 0, transition: { duration: 0.7, ease: [0.16, 1, 0.3, 1] } },
 };
